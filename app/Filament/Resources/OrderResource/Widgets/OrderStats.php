2da7c4e59caeb3084524802b2c5342f8e9945cff
Simon Molina <simon.ramirez7000@gmail.com>
2024-04-24 22:06:41 -0500

    order_stats1

---
+++ app/Filament/Resources/OrderResource/Widgets/OrderStats.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class OrderStats extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('New Orders', Order::query()->where('status', 'new')->count()),
+            Stat::make('Order Processing', Order::query()->where('status', 'processing')->count())
+        ];
+    }
+}
