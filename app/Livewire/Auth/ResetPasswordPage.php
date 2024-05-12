52a75642cd970f263437e4a835f2f317c2f6e3eb
Saymon017 <simon.ramirez7000@gmail.com>
2024-05-11 22:03:34 -0500

    retablecimineto_password

--- app/Livewire/Auth/ResetPasswordPage.php
+++ app/Livewire/Auth/ResetPasswordPage.php
@@ -2,9 +2,16 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Url;
 use Livewire\Component;
+use Illuminate\Support\Str;
+
+use function Laravel\Prompts\password;
 
 #[Title('Reset Password')]
 class ResetPasswordPage extends Component{
@@ -25,6 +32,25 @@ class ResetPasswordPage extends Component{
             'email' => 'required|email',
             'password' => 'required|min:6|confirmed',
         ]);
+
+        $status = Password::reset([
+            'email' => $this->email,
+            'password' => $this->password,
+            'password_confirmation' => $this->password_confirmation,
+            'token' => $this->token,
+        ],
+        function(User $user, string $password){
+            $password = $this->password;
+            $user->forceFill([
+                'password' => Hash::make($password)
+            ])->setRememberToken(Str::random(60));
+            $user->save();
+            event(new PasswordReset($user)); 
+         }
+        );
+
+        return $status === password::PASSWORD_RESET ? redirect('/login') : session()->flash('error', 'something 
+        went wrong');
     }
 
     public function render(){
