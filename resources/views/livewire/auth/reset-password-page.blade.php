3c0f1908780429a484ada42efe285274e66555f3
Saymon017 <simon.ramirez7000@gmail.com>
2024-05-16 00:04:50 -0500

    metodo_pago

--- resources/views/livewire/auth/reset-password-page.blade.php
+++ resources/views/livewire/auth/reset-password-page.blade.php
@@ -10,9 +10,9 @@
             <div class="mt-5">
               <!-- Form -->
               <form wire:submit.prevent='save'>
-                @if (session('success'))
-                    <div class=" bg-gren-500 text-sm text-white rounded-lg p-4" role="alert">
-                        {{ session('success') }}
+                @if (session('error'))
+                    <div class=" bg-red-500 text-sm text-white rounded-lg p-4" role="alert">
+                        {{ session('error') }}
                     </div>
                 @endif
                 <div class="grid gap-y-4">
@@ -20,16 +20,16 @@
                   <div>
                     <label for="password" class="block text-sm mb-2 dark:text-white">Password</label>
                     <div class="relative">
-                      <input type="password" id="password" wire:model="password" class="py-3 px-4 block w-full 
-                      border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 
-                      disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 
+                      <input type="password" id="password" wire:model="password" class="py-3 px-4 block w-full
+                      border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500
+                      disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700
                       dark:text-gray-400 dark:focus:ring-gray-600" required aria-describedby="email-error">
                       @error('password')
                         <div class="absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
-                            <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" 
+                            <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor"
                             viewBox="0 0 16 16" aria-hidden="true">
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.
-                            552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 
+                            552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0
                             0-2z" />
                             </svg>
                         </div>
@@ -44,18 +44,18 @@
                   <div>
                     <label for="password_confirmation" class="block text-sm mb-2 dark:text-white">confirmar Contraseña</label>
                     <div class="relative">
-                      <input type="password" id="password_confirmation" wire:model="password_confirmation" 
-                      class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm 
-                      focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 
-                      disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 
+                      <input type="password" id="password_confirmation" wire:model="password_confirmation"
+                      class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm
+                      focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50
+                      disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400
                       dark:focus:ring-gray-600" aria-describedby="email-error">
 
                       @error('password_confirmation')
                         <div class="absolute inset-y-0 end-0 flex items-center pointer-events-none pe-3">
-                            <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor" 
+                            <svg class="h-5 w-5 text-red-500" width="16" height="16" fill="currentColor"
                             viewBox="0 0 16 16" aria-hidden="true">
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.
-                            552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 
+                            552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0
                             0-2z" />
                             </svg>
                         </div>
